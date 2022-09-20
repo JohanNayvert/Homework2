98f8e38b96be2c4713bc1bb7460c2e5f6647adf5
JohanNayvert <presnetsov.evgeniy@gmail.com>
2022-09-21 00:19:36 +0500

    Complete HomeWork_2

--- com.skyeng/Main.java
+++ com.skyeng/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        // task 1
-        System.out.println("task 1");
+        // Task 1
+        System.out.println("Task 1");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -12,8 +12,8 @@ public class Main {
         System.out.println(paper);
         System.out.println(" ");
 
-        // task 2
-        System.out.println("task 2");
+        // Task 2
+        System.out.println("Task 2");
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
@@ -22,8 +22,8 @@ public class Main {
         System.out.println(paper);
         System.out.println(" ");
 
-        // task 3
-        System.out.println("task 3");
+        // Task 3
+        System.out.println("Task 3");
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
@@ -32,8 +32,8 @@ public class Main {
         System.out.println(paper);
         System.out.println(" ");
 
-        // task 4
-        System.out.println("task 4");
+        // Task 4
+        System.out.println("Task 4");
         var friend = 19;
         System.out.println(friend);
         friend = friend + 2;
@@ -42,8 +42,8 @@ public class Main {
         System.out.println(friend);
         System.out.println(" ");
 
-        // task 5
-        System.out.println("task 5");
+        // Task 5
+        System.out.println("Task 5");
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
@@ -54,8 +54,8 @@ public class Main {
         System.out.println(frog);
         System.out.println(" ");
 
-        // task 6
-        System.out.println("task 6");
+        // Task 6
+        System.out.println("Task 6");
         var oneBoxerWeight = 78.2;
         var twoBoxerWeight = 82.7;
         var generalWeight = oneBoxerWeight + twoBoxerWeight;
@@ -64,27 +64,27 @@ public class Main {
         System.out.println("Разница в весе состовляет " + differenceWeight + " кг");
         System.out.println(" ");
 
-        // task 7
-        System.out.println("task 7");
+        // Task 7
+        System.out.println("Task 7");
         var differenceWeight1 = (twoBoxerWeight - oneBoxerWeight) % generalWeight;
         var differenceWeight2 = twoBoxerWeight - oneBoxerWeight;
         System.out.println("Разница в весе (вычитание) состовляет " + differenceWeight1 + " кг");
         System.out.println("Разница в весе (остаток от деления) состовляет " + differenceWeight2 + " кг");
         System.out.println(" ");
 
-        // task 8
-        System.out.println("task 8");
+        // Task 8.1
+        System.out.println("Task 8.1");
         var workHours = 640;
         var workerHours = 8;
         var amountWorker = workHours / workerHours;
-        System.out.println("Количество сотрудников " + amountWorker);
+        System.out.println("Всего работников в компании –" + amountWorker + " человек");
         System.out.println(" ");
 
-        // task 9
-        System.out.println("task 9");
+        // Task 8.2
+        System.out.println("Task 8.2");
         amountWorker = amountWorker + 94;
         var actualWorkHours = amountWorker * 8;
-        System.out.println("Количество работочасов при увеличении штата " + actualWorkHours + " часов");
+        System.out.println("Если в компании работает " + amountWorker + " человек, то всего " + actualWorkHours + " часов работы может быть поделено между сотрудниками");
         System.out.println("Complete HomeWork_2");
     }
 }
